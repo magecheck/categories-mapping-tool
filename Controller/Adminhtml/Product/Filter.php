03c31858d051b2cc4933e373a68caf2c5addd544
Victor Chiriac <victorchiriac89@gmail.com>
2018-06-08 01:48:27 +0300

    update

--- Controller/Adminhtml/Product/Filter.php
+++ Controller/Adminhtml/Product/Filter.php
@@ -65,52 +65,57 @@ class Filter extends \Magento\Backend\App\Action
      * @param object $attribute
      * @return string
      */
-    public function generateHtml($attribute) {
-        $output = '<label for="'.$attribute->getAttributeCode().'">'.$attribute->getFrontendLabel().'</label>';
-        switch ($attribute->getFrontendInput()){
+    public function generateHtml($attribute)
+    {
+        $output = '<div data-filter="' . $attribute->getId() . '">';
+        $output .= '<label for="' . $attribute->getAttributeCode() . '">';
+        $output .= '<a href="javascript:void(0);" data-remove-filter>&times;</a>&nbsp;';
+        $output .= $attribute->getFrontendLabel();
+        $output .= '</label>';
+        switch ($attribute->getFrontendInput()) {
             case 'multiselect':
                 $options = $this->getAttributeOptions($attribute);
-                if(count($options) > 1){
-                    $output .= '<select class="filter admin__control-multiselect" name="'.$attribute->getAttributeCode().'[]" multiple>';
+                if (count($options) > 1) {
+                    $output .= '<select class="filter admin__control-multiselect" name="' . $attribute->getAttributeCode() . '[]" multiple>';
                     foreach ($options as $option) {
-                        if(isset($option['value'])){
-                            $output .= '<option value="'.$option['value'].'">'.$option['label'].'</option>';
+                        if (isset($option['value'])) {
+                            $output .= '<option value="' . $option['value'] . '">' . $option['label'] . '</option>';
                         }
                     }
                     $output .= '</select>';
-                }else{
-                    $output .= '<span class="no-options">'.__('Dropdown does not have any options!').'</span>';
+                } else {
+                    $output .= '<span class="no-options">' . __('Dropdown does not have any options!') . '</span>';
                 }
                 break;
             case 'select':
                 $options = $this->getAttributeOptions($attribute);
-                if(count($options) > 1){
-                    $output .= '<select class="filter admin__control-select" name="'.$attribute->getAttributeCode().'[select]">';
+                if (count($options) > 1) {
+                    $output .= '<select class="filter admin__control-select" name="' . $attribute->getAttributeCode() . '[select]">';
                     foreach ($options as $option) {
-                        if(isset($option['value'])){
-                            $output .= '<option value="'.$option['value'].'">'.$option['label'].'</option>';
+                        if (isset($option['value'])) {
+                            $output .= '<option value="' . $option['value'] . '">' . $option['label'] . '</option>';
                         }
                     }
                     $output .= '</select>';
-                }else{
-                    $output .= '<span class="no-options">'.__('Dropdown does not have any options!').'</span>';
+                } else {
+                    $output .= '<span class="no-options">' . __('Dropdown does not have any options!') . '</span>';
                 }
-                
+
                 break;
             case 'boolean':
-                $output .= '<select class="filter admin__control-select" name="'.$attribute->getAttributeCode().'[select]">';
-                    $output .='<option value="0">No</option>';
-                    $output .='<option value="1">Yes</option>';
+                $output .= '<select class="filter admin__control-select" name="' . $attribute->getAttributeCode() . '[select]">';
+                $output .=  '<option value="0">No</option>';
+                $output .=  '<option value="1">Yes</option>';
                 $output .= '</select>';
                 break;
             case 'text':
-                $output .= '<input class="filter admin__control-text" type="text" name="'.$attribute->getAttributeCode().'"/>';
+                $output .= '<input class="filter admin__control-text" type="text" name="' . $attribute->getAttributeCode() . '"/>';
                 break;
             case 'textarea':
-                $output .= '<input class="filter admin__control-text" type="text" name="'.$attribute->getAttributeCode().'"/>';
+                $output .= '<input class="filter admin__control-text" type="text" name="' . $attribute->getAttributeCode() . '"/>';
                 break;
         }
-        echo $output . "<br/><br/>";
+        echo $output . "<br /><br /></div>";
     }
     
     /** Get Attribute Options
